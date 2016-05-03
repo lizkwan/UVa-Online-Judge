2c30838eeb597dd945efbd510e3cde1c4a0a03b8
lizkwan <elizabeth@Elizabeths-MacBook-Pro-3.local>
2016-05-02 22:12:23 -0400

    11172

---
+++ 11172.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main ()
+{
+	int a,b,n,i;
+	scanf ("%d",&n);
+	for (i=0; i<n; i++)
+	{
+		scanf ("%d %d",&a,&b);
+		if (a>b)
+			printf (">\n");
+		else if (a<b)
+			printf ("<\n");
+		else
+			printf ("=\n");
+	}
+}
