0f612eaa8467e948667f3fa8df661c3b132e5a0b
Elizabeth Kwan <lizkwan@users.noreply.github.com>
2019-08-04 13:21:40 +0100

    Add files via upload

---
+++ 382.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int in;
+    
+    printf("PERFECTION OUTPUT\n");
+    while(scanf("%d", &in) && (in != 0))
+    {
+        int total = 0;
+        
+        for (int i=1; i<in; i++)
+        {
+            if ((in % i) == 0)
+            {
+                total += i;
+            }
+        }
+        
+        printf("%5d  ", in);
+        
+        if (total == in)
+        {
+            printf("PERFECT\n");
+        }
+        else if (total < in)
+        {
+            printf("DEFICIENT\n");
+        }
+        else if (total > in)
+        {
+            printf("ABUNDANT\n");
+        }
+    }
+    printf("END OF OUTPUT\n");
+    
+    return 0;
+}
